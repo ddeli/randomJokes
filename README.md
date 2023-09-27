4d7b94c177ba9db70fc13722f75e276902c07349
Deniz Deli <d.deli@campus.tu-berlin.de>
2023-09-27 18:44:32 +0200

    Create logic and setup routes. Implement raw frontend

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Access a public joke API an get a random joke
 
 # To run:
-git clone https://www.github.com/ddeli/randomJokes - to clone the project locally
-npm i  - to install all dependencies
-node ./index.js - to start the server
+* git clone https://www.github.com/ddeli/randomJokes 
+* yarn install  
+* yarn start 
--- index.js
+++ index.js
@@ -2,11 +2,26 @@ import express from "express"
 import axios from "axios"
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
+const API_URL = "https://official-joke-api.appspot.com";
+
+// Use public folder for static files.
+app.use(express.static("public"));
+
+// 4. When the user goes to the home page it should render the index.ejs file.
 app.get("/",(req,res) =>{
     res.render("index.ejs");
 })
+
+app.get("/bringMeMyJoke", async (req, res) => {
+    try {
+        const result = await axios.get(API_URL + "/random_joke");
+        res.render("index.ejs", { content: result.data});
+      } catch (error) {
+        res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+      }
+  });
 
 app.listen(port,() => {
     console.log(`Server started and listening on port ${port}`);
